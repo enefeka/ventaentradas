6150c992a9ea6d465dd8544967d0dc72259a21bb
Miguel Heredia Frias <migui.nfk@gmail.com>
2018-11-26 20:55:00 +0100

    Primer commit

--- ventaEntradas/ViewController.swift
+++ ventaEntradas/ViewController.swift
@@ -1,18 +1,9 @@
-//
-//  ViewController.swift
-//  ventaEntradas
-//
-//  Created by Miguel Heredia Frias on 26/11/2018.
-//  Copyright © 2018 Miguel Heredia Frias. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
---
+++ ventaEntradas/detalleCompra.swift
@@ -0,0 +1,30 @@
+//
+//  detalleCompra.swift
+//  ventaEntradas
+//
+//  Created by Miguel Heredia Frias on 26/11/2018.
+//  Copyright © 2018 Miguel Heredia Frias. All rights reserved.
+//
+
+import UIKit
+
+class detalleCompra: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
